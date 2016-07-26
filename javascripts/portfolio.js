8f66b4daed100f3c08b407aeb886cfdd6012073c
Brian Rasmussen <raz5101@users.noreply.github.com>
2016-07-26 16:47:48 -0400

    Add files via upload

---
+++ javascripts/portfolio.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	$('.nav-item').click(function(){
+		$('.nav-item').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	$('body').scrollspy({target: '#myNavbar'});
+
+	$('#inputEmail').validate();
+
+	$('.send-email').click(function(){
+		alert('No message has been sent.  This feature is currently disabled');
+	});
+
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  			var target = $(this.hash);
+  			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  			if (target.length) {
+    			$('html, body').animate({
+      				scrollTop: target.offset().top
+				}, 1000);
+    		return false;
+  			}
+		}
+	});
+});
